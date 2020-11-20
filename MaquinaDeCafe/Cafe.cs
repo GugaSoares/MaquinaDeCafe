7de543f4a8ba512b8acd568836fd7c1f4ee96808
GugaSoares <gustavo.15soares@hotmail.com>
2020-11-20 09:12:01 -0300

    First commit

---
+++ MaquinaDeCafe/Cafe.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaquinaDeCafe
+{
+    class Cafe
+    {
+        public double Saldo { get; private set; }
+        public double Cappuccino { get; private set; }
+        public double Mocha { get; private set; }
+        public double CafeComLeite { get; private set; }
+
+        public Cafe (double saldo, double cappuccino, double mocha, double cafeComLeite)
+        {
+            Saldo = saldo;
+            Cappuccino = cappuccino;
+            Mocha = mocha;
+            CafeComLeite = cafeComLeite;
+        }
+
+
+        public double SaldoDeCompra(double moeda5, double moeda10, double moeda25, double moeda50, double moeda100)
+        {
+            moeda5 *= 0.5;
+            moeda10 *= 0.10;
+            moeda25 *= 0.25;
+            moeda50 *= 0.50;
+            moeda100 *= 1.0;
+            double somaDasMoedas = moeda5 + moeda10 + moeda25 + moeda50 + moeda100;
+            Saldo = Saldo + somaDasMoedas;
+            return Saldo;
+        }
+        public void CafeTroco (string cafeSelecionado)
+        {
+            cafeSelecionado.ToLower();
+            if (cafeSelecionado == "cappuccino")
+            {
+                Saldo -= Cappuccino;
+            }
+            else if (cafeSelecionado == "mocha")
+            {
+                Saldo -= Mocha;
+            }
+            else if (cafeSelecionado == "cafe com leite" || cafeSelecionado == "cafecomleite")
+            {
+                Saldo -= CafeComLeite;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ MaquinaDeCafe/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Globalization;
+
+namespace MaquinaDeCafe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Olá, que tal um cafézinho hoje?");
+            Console.WriteLine();
+            Console.WriteLine("Esta maquina aceita moedas de: R$0.1, R$0.5, R$0.10, R$0.25, R$0.50, R$1.0");
+            Console.WriteLine();
+
+            Console.Write("Insira aqui a quantidade de moedas de R$0.1 centavo: ");
+            double moeda1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (moeda1 >= 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Erro na leitora de moedas! (Esta maquina temporariamente não aceita este valor em moeda)");
+                Console.Write("Deseja continuar (Sim / Não)? ");
+                string resp = Console.ReadLine();
+                if (resp == "Sim" || resp == "sim")
+                {
+
+                }
+                else
+                {
+                    return;
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Insira aqui a quantidade de moedas de R$0.5 centavos: ");
+            double moeda5 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (moeda5 >= 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Erro na leitora de moedas! (Esta maquina temporariamente não aceita este valor em moeda)");
+                Console.Write("Deseja continuar (Sim / Não)? ");
+                string resp = Console.ReadLine();
+                if (resp == "Sim" || resp == "sim")
+                {
+
+                }
+                else
+                {
+                    return;
+                }
+            }
+            Console.WriteLine();
+
+            Console.Write("Insira aqui a quantidade de moedas de R$0.10 centavos: ");
+            double moeda10 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.Write("Insira aqui a quantidade de moedas de R$0.25 centavos: ");
+            double moeda25 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.Write("Insira aqui a quantidade de moedas de R$0.50 centavos: ");
+            double moeda50 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.Write("Insira aqui a quantidade de moedas de R$1.0 centavos: ");
+            double moeda100 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Cafe c = new Cafe(0.0, 3.5, 4.0, 3.0);
+
+            double saldoDeCompra = c.SaldoDeCompra(moeda5, moeda10, moeda25, moeda50, moeda100);
+            Console.WriteLine("Seu saldo de compra é: " + c.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+            if (c.Saldo >= 4.0)
+            {
+                Console.WriteLine("Cafés disponiveis para compra: ");
+                Console.WriteLine("Cappuccino (R$ 3.5)");
+                Console.WriteLine("Mocha (R$ 4.0)");
+                Console.WriteLine("Café com leite (R$ 3.0)");
+            }
+            else if (c.Saldo >= 3.5)
+            {
+                Console.WriteLine("Cafés disponiveis para compra: ");
+                Console.WriteLine("Cappuccino (R$ 3.5)");
+                Console.WriteLine("Café com leite (R$ 3.0)");
+            }
+            else if (c.Saldo >= 3.0)
+            {
+                Console.WriteLine("Café disponivel para compra: ");
+                Console.WriteLine("Café com leite (R$ 3.0)");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para a compra de café");
+                return;
+            }
+            Console.WriteLine();
+            Console.Write("Qual café você deseja? ");
+            string cafeCliente = Console.ReadLine();
+            c.CafeTroco(cafeCliente);
+            Console.WriteLine();
+            Console.WriteLine($"Ok, você escolheu o café {cafeCliente}, aguarde enquanto preparamos para você...");
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(5000);
+            if (c.Saldo == 0.0)
+            {
+                Console.WriteLine("Está pronto seu café, aproveite!!!");
+            }
+            else
+            {
+                Console.WriteLine("Está pronto seu café, e seu troco é: " + c.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
